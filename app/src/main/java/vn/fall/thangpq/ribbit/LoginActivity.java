18c436f9ad45db8d17bfb9ac5a6a7c06c0d08eec
ThangPQ <thangpq@outlook.com.vn>
2015-01-06 20:02:39 +0700

    Add link to SignUp Activity in "Sign Up" text

--- app/src/main/java/vn/fall/thangpq/ribbit/LoginActivity.java
+++ app/src/main/java/vn/fall/thangpq/ribbit/LoginActivity.java
@@ -1,17 +1,29 @@
 package vn.fall.thangpq.ribbit;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class LoginActivity extends ActionBarActivity {
 
+    protected TextView mSignUpText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mSignUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
